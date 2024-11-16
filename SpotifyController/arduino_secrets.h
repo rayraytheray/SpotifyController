2b6ad224986391eb5b90fa16a7619ca0c26ba875
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-11-16 11:31:24 -0500

    updated variables

--- SpotifyController/arduino_secrets.h
+++ SpotifyController/arduino_secrets.h
@@ -1,2 +1,9 @@
 #define SECRET_SSID "Brown-Guest"
 #define SECRET_PASS ""
+
+#define SPOTIFY_CLIENT_ID "ce52c9f4c06d4e24857ce7d900979937"          
+#define SPOTIFY_CLIENT_SECRET "420dcc8a179f43b39cd1d757453dd6b4"  
+#define SPOTIFY_REFRESH_TOKEN "your-refresh-token"  // need to replace
+
+// Spotify Market (Country Code)
+#define SPOTIFY_MARKET "US" // Replace with your country code
