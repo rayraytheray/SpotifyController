46f34e4565b4a23bf32677eca001bbed1da2062c
rayraytheray <raymond_kao@brown.edu>
2024-12-09 19:11:41 -0500

    Add watchdog timer and button interrupts

--- SpotifyController/SpotifyController.ino
+++ SpotifyController/SpotifyController.ino
@@ -1,14 +1,48 @@
 #include "SpotifyController.h"
 #include "SpotifyUtils.h"
 #include "io_utils.h"
+#include <FspTimer.h>
+
+FspTimer watchdogTimer;
+const int playPin = 2;  // play/pause button
+const int skipPin = 3;  // skip button
+volatile bool playFlag = false;
+volatile bool skipFlag = false;
 
 void setup() {
-  Serial.begin(115200); Serial.println();
+  // Set up serial
+  Serial.begin(115200); 
+  Serial.println();
+
   Serial.println("OK"); // let the python code know we are ready
+
+  // Configure watchdog timer 
+  uint32_t period_counts = 1000000; // 1 second in microseconds
+  if (!watchdogTimer.begin(TIMER_MODE_PERIODIC, AGT_TIMER, 0, period_counts, 0, TIMER_SOURCE_DIV_8, wdtISR)) {
+    Serial.println("Watchdog Timer configuration failed!");
+    return;
+  }
+  // Start the timer
+  watchdogTimer.start();
+
+  // Set up LCD and buttons
   setupUtils();
+
+  // set up button pin interrupt
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
 }
 
 void loop() {
+  // Pet watchdog
+  watchdogTimer.stop();
+  watchdogTimer.start();
+
+  if (interruptFlag) {
+    Serial.println("Interrupt triggered!");
+    interruptFlag = false;
+  }
+
   // echo back in uppercase what we received
   if (Serial.available()) {
     writeToBuf();
@@ -26,7 +60,6 @@ void loop() {
     
   int potValue = analogRead(A0); // Read potentiometer value (0-1023)
   Serial.println(potValue);     // Send value to the computer
-  delay(100); 
 }
 
 state updateFSM(state curState, long mils, int lastButton) {
@@ -46,4 +79,14 @@ state updateFSM(state curState, long mils, int lastButton) {
     break;
   }
   return nextState;
+}
+
+void handleInterrupt() {
+  interruptFlag = true;
+}
+
+/* ISR when WDT triggers */
+void wdtISR(timer_callback_args_t *arg) {
+  Serial.println("System failure detected!");
+  while(true);
 }
